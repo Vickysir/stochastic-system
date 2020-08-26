5eb0f4d70a922dc5cfc6914f32527cf42f1f963c
Vickysir <460022058@qq.com>
2020-08-26 13:15:34 +0000

    Deploying to deploy from  @ 85324eed753075faa65357e30283ade5fca41a35 🚀

---
+++ precache-manifest.831c36a011f198777523baa2ff480ea4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f34071155d06e5e064cae26236dcb75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ce8a413196e5621fac4",
+    "url": "/static/css/main.c8a6df4b.chunk.css"
+  },
+  {
+    "revision": "2a251ac2efe06ade7e06",
+    "url": "/static/js/2.f17e1e36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f17e1e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce8a413196e5621fac4",
+    "url": "/static/js/main.a79e2a60.chunk.js"
+  },
+  {
+    "revision": "a06eac904de6dd3815e1",
+    "url": "/static/js/runtime-main.6b7a4147.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
